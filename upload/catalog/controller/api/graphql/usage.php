5566bac33eb5d861bf2b0b63d0f372aa6f8fdb2c
YasienDwieb <yasien.dwieb@gmail.com>
2019-11-21 14:09:57 +0200

    improve usage file

--- upload/catalog/controller/api/graphql/usage.php
+++ upload/catalog/controller/api/graphql/usage.php
@@ -1,20 +1,18 @@
 <?php
-use GraphQL\GraphQL;
 
 require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__.'/Resolvers/Helpers.php';
-require_once __DIR__.'/Types.php';
+
+use GraphQL\GraphQL;
+use GQL\Types;
+use GQL\Helpers\Utils;
 
 define ('GQ_INTERNAL_KEY', '__GQ_INTERNAL_KEY__');
-define ('GQ_PLUGIN_VERSION', '1.0');
+define ('GQ_PLUGIN_VERSION', '1.129');
 
 class ControllerApiGraphqlUsage extends Controller {
 	public $sess = "";
-
-	function index () {
-	    
-		error_reporting (0);
-		$this->sess = $this->session->getId();
+	public function index () {
+		error_reporting(0);
 		$rawBody = "";
 		if (!isset($this->request->get['token']) && false){
 			$result = [
@@ -23,22 +21,17 @@ class ControllerApiGraphqlUsage extends Controller {
 				]
 			];
 		} else{
-			$token=$this->request->get['token'];
-			// echo $token ;
-			
-			$this->load->model('account/api');
-			$api_info = "";//$this->model_account_api->getApiByKey($token);
-
-// print_r($api_info);
-
-			if(!$api_info && false){
-				$result = [
-					'error' => [
-					    'message' => 'Not Authorized'
-					]
-				];
+			// $token=$this->request->get['token'];
+			// $this->load->model('account/api');
+			// $api_info = $this->model_account_api->getApiByKey($token);
+			// if(!$api_info && false){
+			// 	$result = [
+			// 		'error' => [
+			// 		    'message' => 'Not Authorized'
+			// 		]
+			// 	];
+			if(false){
 			}else{
-
 				try {
 					// Set a session and send it back.
 					$headers = getallheaders ();
@@ -51,38 +44,21 @@ class ControllerApiGraphqlUsage extends Controller {
 					}
 
 					if (!empty ($this->sess)){
-						$this->sess = \GQL\getSession ($this, $this->sess);
+						$this->sess = Utils::getSession ($this, $this->sess);
 						header("x-session-id: {$this->sess}");
 					}
-					
+
 					$rawBody = file_get_contents('php://input');
-					
 					$data = json_decode($rawBody ?: '', true);
-					
 					$requestString = isset($data['query']) ? $data['query'] : null;
 					$operationName = isset($data['operation']) ? $data['operation'] : null;
 					$variableValues = isset($data['variables']) ? $data['variables'] : null;
 					if (!(is_object ($variableValues) || is_array($variableValues)))
 						$variableValues = json_decode($variableValues, True);
-					$types = GQL\Types::Instance ();
-					// var_dump($types);
+					$types = Types::Instance ();
 					$rootValue = ['prefix' => 'You said: '];
-					// print_r($schema);
-					// print_r($requestString);
-					// print_r($variableValues);
-					
-                    //$db = new DB('mysqli', $this->config->get('db_hostname'), $this->config->get('db_username'), $this->config->get('db_password'), $this->config->get('db_database'), $this->config->get('db_port'));
-                    //$req = json_encode([$requestString,$rawBody,$_REQUEST,$_GET,$_POST]);
-                    //$res =  $this->db->query("INSERT INTO `requests`(`request`) VALUES ('$req')");
-
-
 					$result = GraphQL::execute($types::$schema, $requestString, null, $this, $variableValues);
-					// print_r($result);
-					// header('Content-Type: application/json; charset=UTF-8');
-					// print_r( json_encode($result));
-					
 				} catch (\Exception $e) {
-					
 					$result = [
 						'error' => [
 						    'message' => $e
@@ -92,11 +68,7 @@ class ControllerApiGraphqlUsage extends Controller {
 			}
 		}
 		header('Content-Type: application/json; charset=UTF-8');
-		// print_r( json_encode($result));
-		// $res =json_encode($result)->data;
-		// print_r($res);
 		echo json_encode($result);
-		
 	}
 }
 
